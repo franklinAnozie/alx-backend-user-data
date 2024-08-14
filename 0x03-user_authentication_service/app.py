c3e36c991d34d44a101a09a913dda05dc028cce5
franklinAnozie <cfanozie@gmail.com>
2024-08-14 16:39:54 +0100

    attempted more tasks

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python3
 """ flask app """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort, redirect
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"], strict_slashes=False)
 def home():
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users():
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    return jsonify({"email": user.email, "message": "user created"})
 
 
 if __name__ == "__main__":
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+import uuid
 
 
 class Auth:
@@ -25,7 +26,29 @@ class Auth:
         user = self._db.add_user(email, hashed_password)
         return user
 
+    def valid_login(self, email: str, password: str) -> bool:
+        """ login validator """
+        try:
+            user = self._db.find_user_by(email=email)
+            pass_check = bcrypt.checkpw(password, user.hashed_password)
+        except Exception as e:
+            return False
+        return pass_check
+    
+    def create_session(self, email: str) -> str:
+        """ creates a user login session """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+        session_id = _generate_uuid()
+        self._db.update_user(user.id, session_id=session_id)
+        return session_id
+
 
 def _hash_password(password: str) -> bytes:
     """ hash fxn """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+def _generate_uuid() -> str:
+    return str(uuid.uuid4())
