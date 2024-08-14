29af1421e2512c1949634a5fc4349514606ac308
franklinAnozie <cfanozie@gmail.com>
2024-08-15 00:09:30 +0100

    testing fix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -77,7 +77,7 @@ class Auth:
         except NoResultFound:
             raise ValueError
         hashed_password = _hash_password(password)
-        self._db.update_user(user, hashed_password=hashed_password,
+        self._db.update_user(user.id, hashed_password=hashed_password,
                              reset_token=None)
 
 
