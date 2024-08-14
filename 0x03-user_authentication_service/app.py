cd3772f326f7280cb88ebda8f0c08c182e8ce63e
franklinAnozie <cfanozie@gmail.com>
2024-08-14 23:54:51 +0100

    testing fix

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -17,13 +17,18 @@ def home():
 @app.route("/users", methods=["POST"])
 def users():
     """ create new user """
-    email = request.form.get("email")
-    password = request.form.get("password")
+    try:
+        email = request.form.get("email")
+        password = request.form.get("password")
+    except KeyError:
+        abort(400)
+
     try:
         user = AUTH.register_user(email, password)
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
-    return jsonify({"email": user.email, "message": "user created"})
+
+    return jsonify({"email": email, "message": "user created"})
 
 
 @app.route("/sessions", methods=["POST"])
