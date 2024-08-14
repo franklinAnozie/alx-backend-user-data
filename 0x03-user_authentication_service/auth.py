1cb64be06dd997977e87406d1bac339397291e79
franklinAnozie <cfanozie@gmail.com>
2024-08-14 23:45:54 +0100

    testing fix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -19,13 +19,13 @@ class Auth:
     def register_user(self, email: str, password: str):
         """ register a new user """
         try:
-            self._db.find_user_by(email=email)
-            raise ValueError(f"User {email} already exists")
+            user = self._db.find_user_by(email=email)
         except NoResultFound:
-            pass
-        hashed_password = _hash_password(password)
-        user = self._db.add_user(email, hashed_password)
-        return user
+            hashed_password = _hash_password(password)
+            user = self._db.add_user(email, hashed_password)
+            return user
+        else:
+            raise ValueError(f"User {email} already exists")
 
     def valid_login(self, email: str, password: str) -> bool:
         """ login validator """
