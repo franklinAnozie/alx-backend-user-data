d964b9b3874ccdd138984040dba88e65163e53b5
franklinAnozie <cfanozie@gmail.com>
2024-08-14 23:46:41 +0100

    testing fix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -16,7 +16,7 @@ class Auth:
         """ init """
         self._db = DB()
 
-    def register_user(self, email: str, password: str):
+    def register_user(self, email: str, password: str) -> User:
         """ register a new user """
         try:
             user = self._db.find_user_by(email=email)
