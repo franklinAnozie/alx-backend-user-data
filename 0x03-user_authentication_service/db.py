167e32545a0c6506c01590a059d8257821fc2e8a
franklinAnozie <cfanozie@gmail.com>
2024-08-14 12:50:15 +0100

    changes made

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -41,7 +41,7 @@ class DB:
         self._session.commit()
         return user
 
-    def find_user_by(self, **kwargs: dict) -> User:
+    def find_user_by(self, **kwargs) -> User:
         """ find a user by search parameters """
         if not kwargs:
             raise InvalidRequestError
