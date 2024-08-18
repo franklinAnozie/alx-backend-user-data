7c72997ea08dba2e1fb2b64083b3491cf9b4b34f
franklinAnozie <cfanozie@gmail.com>
2024-08-18 14:06:28 +0100

    auth not necessary

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -21,3 +21,12 @@ class Auth():
     def current_user(self, request=None) -> TypeVar('User'):
         """ current_user """
         return None
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ auth not required """
+        if not path.endswith('/'):
+            path = f"{path}/"
+        if (path is None | excluded_paths is None
+           | len(excluded_paths) == 0 | path not in excluded_paths):
+            return True
+        return False
